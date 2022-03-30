3aac4407633d941237dd1573654f7d1b25043c56
Vladimir <gorban469@gmail.com>
2022-03-30 18:06:10 +0300

    Change functions positions

--- TicTacToe.js
+++ TicTacToe.js
@@ -3,13 +3,8 @@ let cardBody = document.getElementById('cardBody')
 let alerts = document.getElementById('alerts')
 let gameMatrix = []
 
-function play () {
-   let audio = document.getElementById('audio')
-    audio.play()
-}
-
-function printMessage (alertType, message) {
-    alerts.innerHTML = `<div class="alert alert-${alertType}" role="alert">${message}</div>`
+for (let i = 0; i < 3; ++i) {
+    gameMatrix[i] = []
 }
 
 const player1 = {
@@ -24,43 +19,6 @@ const player2 = {
     putSign(section) {
         completeSectionAndGameMatrix(section, player2.name, '0')
     }
-}
-
-for (let i = 0; i < 3; ++i) {
-    gameMatrix[i] = []
-}
-
-function createRestartButton () {
-    cardBody.innerHTML = '<button type="button" class="btn btn-secondary" onclick="restartGame()">Restart</button>'
-}
-
-function restartGame () {
-    location.reload()
-}
-
-function completeSectionAndGameMatrix (section, name, signature) {
-    document.getElementById(`${section}`).innerHTML = signature
-    const i = (section / 10).toFixed()
-    const j = (section % 10).toFixed()
-    gameMatrix[i][j] = name
-}
-
-function checkWinner () {
-    for (let i = 0; i < 3; ++i) {
-        if (gameMatrix[i][0] === gameMatrix[i][1] && gameMatrix[i][1] === gameMatrix[i][2]) {
-            return  gameMatrix[i][0]
-        }
-        if (gameMatrix[0][i] === gameMatrix[1][i] && gameMatrix[1][i] === gameMatrix[2][i]) {
-            return gameMatrix[0][i]
-        }
-    }
-    if (gameMatrix[0][0] === gameMatrix[1][1] && gameMatrix[1][1] === gameMatrix[2][2]) {
-        return gameMatrix[0][0]
-    }
-    if (gameMatrix[0][2] === gameMatrix[1][1] && gameMatrix[1][1] === gameMatrix[2][0]) {
-        return gameMatrix[0][2]
-    }
-    return false
 }
 
 let iterationsSignatures = 0
@@ -80,14 +38,6 @@ function submitPlayer () {
     } else {
         printMessage('warning', 'Enter a valid name')
     }
-}
-
-function whoseTurn (name) {
-    cardBody.innerHTML = `<h5>${name} turn</h5>`
-}
-
-function sectionWasNotSelected (section) {
-    return gameMatrix[(section / 10).toFixed()][(section % 10).toFixed()] === undefined;
 }
 
 function putSignPlayer (section) {
@@ -123,6 +73,56 @@ function putSignPlayer (section) {
     } else {
         printMessage('warning', 'First enter you name or click on submit!')
     }
+}
+
+function checkWinner () {
+    for (let i = 0; i < 3; ++i) {
+        if (gameMatrix[i][0] === gameMatrix[i][1] && gameMatrix[i][1] === gameMatrix[i][2]) {
+            return  gameMatrix[i][0]
+        }
+        if (gameMatrix[0][i] === gameMatrix[1][i] && gameMatrix[1][i] === gameMatrix[2][i]) {
+            return gameMatrix[0][i]
+        }
+    }
+    if (gameMatrix[0][0] === gameMatrix[1][1] && gameMatrix[1][1] === gameMatrix[2][2]) {
+        return gameMatrix[0][0]
+    }
+    if (gameMatrix[0][2] === gameMatrix[1][1] && gameMatrix[1][1] === gameMatrix[2][0]) {
+        return gameMatrix[0][2]
+    }
+    return false
+}
+
+function completeSectionAndGameMatrix (section, name, signature) {
+    document.getElementById(`${section}`).innerHTML = signature
+    const i = (section / 10).toFixed()
+    const j = (section % 10).toFixed()
+    gameMatrix[i][j] = name
+}
+
+function whoseTurn (name) {
+    cardBody.innerHTML = `<h5>${name} turn</h5>`
+}
+
+function sectionWasNotSelected (section) {
+    return gameMatrix[(section / 10).toFixed()][(section % 10).toFixed()] === undefined;
+}
+
+function play () {
+    let audio = document.getElementById('audio')
+    audio.play()
+}
+
+function printMessage (alertType, message) {
+    alerts.innerHTML = `<div class="alert alert-${alertType}" role="alert">${message}</div>`
+}
+
+function createRestartButton () {
+    cardBody.innerHTML = '<button type="button" class="btn btn-secondary" onclick="restartGame()">Restart</button>'
+}
+
+function restartGame () {
+    location.reload()
 }
 
 input.addEventListener('keyup', function(event) {
